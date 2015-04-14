02bdfa0fc9b3309b536bb08aa7eaaf49887a80e4
Julien Michel <julien.michel@orfeo-toolbox.org>
2015-04-14 09:25:10 +0200

    ENH: Update module dependencies

--- otb-module.cmake
+++ otb-module.cmake
@@ -1,1 +1,20 @@
-otb_module(AppFeatureSelection DEPENDS  OTBCore DESCRIPTION "Feature selection application contributed by Walid Bouceffa and Nesrine Chehata")
+
+otb_module(AppFeatureSelection
+  DEPENDS
+    OTBApplicationEngine
+    OTBBoost
+    OTBCommon
+    OTBIOXML
+    OTBITK
+    OTBImageBase
+    OTBImageIO
+    OTBLibSVM
+    OTBObjectList
+    OTBProjection
+    OTBStatistics
+    OTBSupervised
+    OTBVectorDataBase
+
+  DESCRIPTION
+    "Feature
+)
